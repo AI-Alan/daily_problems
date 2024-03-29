9522ec5803d7fc146078d0eb2e6379a5ce176b48
aman <amorphiouslattice@gmail.com>
2024-03-29 12:12:43 +0530

    added first fiel

---
+++ return.c
@@ -0,0 +1,9 @@
+// finding if satatements agter return in a functin will be executed or not
+
+#include<stdio.h>
+int main()
+{
+    printf("This is berfore return");
+    return 0;
+    printf("This is after return statement\n");
+}
